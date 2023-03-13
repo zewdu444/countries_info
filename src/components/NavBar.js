503ddc6a30cf226c53362cbcce0b3e29ca399f64
zewdu erkyhun <zewdu444@gmail.com>
2023-03-13 18:13:11 +0300

    Add Nav APP bar

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,8 +1,55 @@
-import React from 'react';
+import * as React from 'react';
+import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
+import IconButton from '@mui/material/IconButton';
+import ArrowBackIosIcon from '@mui/icons-material/ArrowBackIos';
+import SettingsIcon from '@mui/icons-material/Settings';
+import MicNoneIcon from '@mui/icons-material/MicNone';
 
 function NavBar() {
   return (
-    <div>NavBar</div>
+    <Box sx={{ flexGrow: 1 }}>
+      <AppBar position="static" sx={{ bgcolor: 'teal' }}>
+        <Toolbar>
+          <IconButton
+            size="medium"
+            edge="start"
+            color="inherit"
+            aria-label="menu"
+            sx={{ mr: 0 }}
+          >
+            <ArrowBackIosIcon />
+          </IconButton>
+          <Typography variant="h6" component="div">
+            2015
+          </Typography>
+          <Typography variant="h7" component="div" sx={{ flexGrow: 1 }} align="center">
+            most views
+          </Typography>
+
+          <IconButton
+            size="medium"
+            edge="start"
+            color="inherit"
+            aria-label="mic"
+            sx={{ mr: 1 }}
+          >
+            <MicNoneIcon />
+          </IconButton>
+          <IconButton
+            size="medium"
+            edge="start"
+            color="inherit"
+            aria-label="setting"
+            sx={{ mr: 1 }}
+          >
+            <SettingsIcon />
+          </IconButton>
+        </Toolbar>
+      </AppBar>
+    </Box>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import { Provider } from 'react-redux';
 import App from './App';
-import store from './components/redux/store';
+import store from './redux/store';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
